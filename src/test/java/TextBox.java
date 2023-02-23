d51a0bd8bb397243c72c3abd9fdb1badcc4df1e4
Slomako <lomakosv@gmail.com>
2023-02-23 12:16:15 +0300

    added TextBox.java

--- src/test/java/TextBox.java
+++ src/test/java/TextBox.java
@@ -1,14 +1,22 @@
+
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
-import static org.openqa.selenium.remote.tracing.EventAttribute.setValue;
 
-public class TextBox {
+
+public class privet {
     @Test
     void textBoxTest() {
 
         open("https://demoqa.com/text-box");
         $("[Id=userName]").setValue("Sergei Lomako");
+        $("[Id=userEmail]").setValue("lomakosv@gmail.com");
+        $("[Id=currentAddress]").setValue("null");
+        $("[Id=permanentAddress]").setValue("null");
+        $("[Id=submit]").scrollIntoView(true);
+        $("[Id=submit]").click();
+        $("[Id=output]").shouldHave(text("Lomako"), text("@"));
         sleep(5000);
     }
 }
